66c90a36f2f4a100c8434c88c324c3d4c584fefc
Agustingmaggi <agustingmaggi@gmail.com>
2021-11-17 17:34:58 +0000

    agregando css

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const publicPath = path.resolve(__dirname, "./public")
 app.use(express.static(publicPath))
 
 app.set('port',3000)
-app.listen(app.get('port'), ()=> console.log('listening to the port'))
+app.listen(app.get('port'), ()=> console.log('corriendo en el puerto 3000'))
 
 app.get("/", (req, res)=> {
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
