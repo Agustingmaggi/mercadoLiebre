49dadc8bca06ad31e183c38c4832b87d7d9ebfab
Agustingmaggi <agustingmaggi@gmail.com>
2021-12-04 16:47:26 +0000

    version final 1.0.0

--- app.js
+++ app.js
@@ -11,5 +11,8 @@ app.listen(app.get('port'), ()=> console.log('corriendo en el puerto 3000'))
 app.get("/", (req, res)=> {
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
 })
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname,"./views/register.html")))
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname,"./views/login.html")))
+
 
 app.use(express.static(path.resolve(__dirname, "./public")))
